9c69e491533dc8d36d1a69cbac1ef03df7783600
jessicarozario22 <jessicarozario.018gmail.com>
2026-09-13 18:05:12 +0600

    update technologies.tsx file

---
+++ src/components/Technologies/SelectedTechnologies.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedTechnologies = () => {
+    return (
+        <div>
+            Selected Technologies
+        </div>
+    );
+};
+
+export default SelectedTechnologies;
